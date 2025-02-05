bf82b0c4d8b0ac36e2c0d828a628706eeaf9a55c
vickitaylor <vtaylor85@live.com>
2025-02-04 21:05:27 -0600

    value line chart completed

--- property_values/server.R
+++ property_values/server.R
@@ -1,29 +1,69 @@
 
 
-# Define server logic 
+# Define server logic
 function(input, output, session) {
 
-  # dates are only the last day of the month but allowing any day for selection, so need to change the date to find in the table 
-  input_min_date <- ceiling_date(as.Date(input_date[1]), "month") - days(1)
-  input_max_date <- ceiling_date(as.Date(input_date[2]), "month") - days(1)
-  
-   
 
-  
-  
+  # dates are only the last day of the month but allowing any day for selection, so need to change the date to find in the table
+  min_date <- reactive({
+    min_date <- ceiling_date(as.Date(input$date[1]), "month") - days(1)
+  })
+
+  max_date <- reactive({
+    max_date <- ceiling_date(as.Date(input$date[2]), "month") - days(1)
+  })
+
+
+
+  # region to two things, need to rename them to something
+  #
+  output$value_line <- renderPlotly({
+    
+    region_data <- reactive({
+      region <- values
+      
+      if(input$region != 'All'){
+        region <- region |>
+          filter(parent_metro_region == input$region)
+      }
+      else {
+        region <- region |>
+          group_by(period_end) |> 
+          summarize(estimated_value = mean(estimated_value, na.rm = TRUE))
+      }
+    })
+    
+    # region <- 'Nashville, Tn'
+    plot_value <- region_data() |>
+      filter(period_end >= min_date(),
+             period_end <= max_date()) |>
+      ggplot(aes(x = period_end, y = estimated_value)) +
+      geom_line()
+    
+    ggplotly(plot_value)
+  })
+
+
 }
 
 
+# output$distPlot <- renderPlot({
+# 
+#     # generate bins based on input$bins from ui.R
+#     x    <- faithful[, 2]
+#     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+# 
+#     # draw the histogram with the specified number of bins
+#     hist(x, breaks = bins, col = 'darkgray', border = 'white',
+#          xlab = 'Waiting time to next eruption (in mins)',
+#          main = 'Histogram of waiting times')
+# 
+# })
 
-    # output$distPlot <- renderPlot({
-    # 
-    #     # generate bins based on input$bins from ui.R
-    #     x    <- faithful[, 2]
-    #     bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    # 
-    #     # draw the histogram with the specified number of bins
-    #     hist(x, breaks = bins, col = 'darkgray', border = 'white',
-    #          xlab = 'Waiting time to next eruption (in mins)',
-    #          main = 'Histogram of waiting times')
-    # 
-    # })
+
+
+
+
+
+
+
--- property_values/ui.R
+++ property_values/ui.R
@@ -1,37 +1,39 @@
-
-
-
-
 # Define UI for application t
 ui <- page_fluid(
-  
   theme = bs_theme(version = 5, bootswatch = 'morph'),
   
   # Application title
   titlePanel('Tennessee Property Valuations'),
   
   # Sidebar with a date selection input
-  # sidebarLayout(
-    dateRangeInput(
-      inputId = 'input_date',
-      label = 'Select Date Range',
-      start = min_date,
-      end = max_date,
-      format = 'mm/dd/yyyy'
+  sidebarLayout(
+    sidebarPanel(
+      dateRangeInput(
+        inputId = 'date',
+        label = 'Select Date Range',
+        start = min_date,
+        end = max_date,
+        format = 'mm/dd/yyyy'
+      ), 
+      selectInput(
+        inputId = 'region', 
+        label = 'Select Region',
+        choices = c( 'All', values |> distinct(parent_metro_region) |> pull(parent_metro_region) |> sort()),
+        selected = 1
+      )
+    ),
     
+    mainPanel(
+      plotlyOutput(outputId = 'value_line')
     )
-  # ),
-  # mainPanel(
-    # plotOutput("distPlot")
-  # )
+  )
 )
-  
-  
-  # sidebarPanel(
-  #     sliderInput("bins",
-  #                 "Number of bins:",
-  #                 min = 1,
-  #                 max = 50,
-  #                 value = 30)
-  
-  # Show a plot of the generated distribution
+
+# sidebarPanel(
+#     sliderInput("bins",
+#                 "Number of bins:",
+#                 min = 1,
+#                 max = 50,
+#                 value = 30)
+
+# Show a plot of the generated distribution
